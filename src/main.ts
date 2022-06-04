0b14b59a82501fbbb205613a83ecde2d018e7010
Rudy Marchandise <r.marchandise@sfr.fr>
2022-06-04 13:44:42 +0200

    feat(swagger): add endpoints documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const currentPackage = require('../package.json');
+
+/**
+ * Application
+ */
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  await bootstrapSwagger(app);
   await app.listen(process.env.PORT || 8080);
 }
+
+/**
+ * Swagger
+ */
+async function bootstrapSwagger(app: INestApplication): Promise<void> {
+  const appName = currentPackage.name
+    .replace('-', ' ')
+    .split(' ')
+    .map((word: string) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(' ');
+  const config = new DocumentBuilder()
+    .setTitle(appName)
+    .setDescription(currentPackage.description)
+    .setVersion(currentPackage.version)
+    .setLicense(currentPackage.license, currentPackage.licenseUrl)
+    .setExternalDoc('GitHub Repository', currentPackage.homepage)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+}
+
+/**
+ * Run
+ */
 bootstrap();
