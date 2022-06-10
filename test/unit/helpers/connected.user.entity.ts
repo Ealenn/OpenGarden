e19e275ef9099fd8ca8f8bcd488a30286fd60360
Rudy Marchandise <r.marchandise@sfr.fr>
2022-06-10 21:44:11 +0200

    fix(tests): remove unused tests files

--- test/unit/helpers/connected.user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from 'mongoose';
-import { User } from 'src/users/models/user.entity';
-
-export const testingConnectedUser: User = {
-  _id: new Types.ObjectId(),
-  email: 'example@domain.com',
-  password: '123456789',
-  username: 'testUser',
-};
