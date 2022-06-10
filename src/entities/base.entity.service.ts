2e5e130e674cf7a0a1f59fc09236a5e31551a370
Rudy Marchandise <r.marchandise@sfr.fr>
2022-06-11 00:29:09 +0200

    fix(any): avoid using any type

--- src/entities/base.entity.service.ts
+++ src/entities/base.entity.service.ts
@@ -1,5 +1,10 @@
+export interface regexObject {
+  $regex: string;
+}
+
 export class BaseEntityService {
   /**
+   * Escape RegExp special characters
    * @see https://github.com/sindresorhus/escape-string-regexp
    */
   protected _escapeStringRegex(string: string): string {
@@ -8,16 +13,19 @@ export class BaseEntityService {
 
   /**
    * Generate MongoDB Document Filters
-   * @param {Record<string, any>} filters { filterKey: "data" }
-   * @returns {any} { filterKey: { $regex: 'data' } }
+   * @param {Record<string, any>} filters { filterKey: "data", filterUndefined: undefined }
+   * @returns {Record<string, regexObject>} { filterKey: { $regex: 'data' } }
    */
-  protected _generateFilters(filters: Record<string, any>): any {
+  protected _generateFilters(
+    filters: Record<string, any>,
+  ): Record<string, regexObject> {
     const result = {};
     for (const [key, value] of Object.entries(filters)) {
       if (value && typeof value === 'string') {
-        result[key] = {
+        const regexObject: regexObject = {
           $regex: this._escapeStringRegex(value),
         };
+        result[key] = regexObject;
       }
     }
     return result;
