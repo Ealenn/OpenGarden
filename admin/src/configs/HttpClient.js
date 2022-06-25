673ed18be8ae96c8c9f6071e2f22894704438218
Rudy Marchandise <r.marchandise@sfr.fr>
2022-06-25 14:35:16 +0200

    fix(admin): fetch data errors notification

--- admin/src/configs/HttpClient.js
+++ admin/src/configs/HttpClient.js
@@ -1,4 +1,36 @@
-import { fetchUtils } from 'react-admin';
+/* eslint-disable no-unused-vars */
+const fetchJson = async (url, options = {}) => {
+  const requestHeaders = (options.headers ||
+    new Headers({
+      Accept: 'application/json',
+    })
+  );
+  if (!requestHeaders.has('Content-Type') &&
+    !(options && options.body && options.body instanceof FormData)) {
+    requestHeaders.set('Content-Type', 'application/json');
+  }
+  if (options.user && options.user.authenticated && options.user.token) {
+    requestHeaders.set('Authorization', options.user.token);
+  }
+  const response = await fetch(url, { ...options, headers: requestHeaders })
+  const text = await response.text()
+  const o = {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+    body: text,
+  };
+  let status = o.status, statusText = o.statusText, headers = o.headers, body = o.body;
+  let json;
+  try { json = JSON.parse(body); } catch (e) { }
+  if (status < 200 || status >= 300) {
+    console.error(json || body);
+    return Promise.reject({
+      message: statusText
+    });
+  }
+  return Promise.resolve({ status: status, headers: headers, body: body, json: json });
+};
 
 export const HttpClient = (url, options = {}) => {
   if (!options.headers) {
@@ -6,5 +38,6 @@ export const HttpClient = (url, options = {}) => {
   }
   const access_token = JSON.parse(localStorage.getItem("access_token"));
   options.headers.set("Authorization", `Bearer ${access_token}`);
-  return fetchUtils.fetchJson(url, options);
+
+  return fetchJson(url, options);
 };
